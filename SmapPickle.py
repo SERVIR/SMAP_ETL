c0f78ff28c3e405c974a503cde36769e277a9a8d
Lance Gilliland <lance.gilliland@nasa.gov>
2018-03-21 09:47:39 -0500

    Updated to reflect GIS1 as admin server URL.

--- SmapPickle.py
+++ SmapPickle.py
@@ -1,7 +1,17 @@
 import pickle
 import csv
 import binascii
-mydict = {'extract_Folder': 'e:\Temp\SMAP_Extract\\', 'mosaicDS': 'E:\SERVIR\Data\Global\soil_moisture.gdb\soil_moisture', 'finalTranslateFolder': 'E:\SERVIR\Data\Global\soil_moisture', 'logFileDir': 'D:\Logs\ETL_Logs\SMAP','current_AdminDirURL':'http://localhost:6080/arcgis/admin','current_Username':'YOUR_USERNAME','current_Password':'YOUR_PASSWORD','current_FolderName':'Global','current_ServiceName':'SoilMoisture','current_ServiceType':'MapServer'}
+
+mydict = {'extract_Folder': 'E:\Code\SMAP\Global\TempExtractFolder\\',
+          'mosaicDS': 'E:\Code\SMAP\Global\soil_moisture.gdb\soil_moisture',
+          'finalTranslateFolder': 'E:\Code\SMAP\Global\soil_moisture',
+          'logFileDir': 'E:\Code\SMAP_ETL',
+          'current_AdminDirURL': 'https://gis1.servirglobal.net/arcgis/admin',
+          'current_Username': 'YOUR_USERNAME',
+          'current_Password': 'YOUR_PASSWORD',
+          'current_FolderName': 'Global',
+          'current_ServiceName': 'SoilMoisture',
+          'current_ServiceType': 'MapServer'}
 output = open('config.pkl', 'wb')
 pickle.dump(mydict, output)
 output.close()
